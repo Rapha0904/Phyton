88ff9ff35129ab51b7f1d5703670432c034cd266
Rapha0904 <159035933+Rapha0904@users.noreply.github.com>
2025-09-24 08:55:27 -0300

    Update generos_tb.py

--- generos_tb.py
+++ generos_tb.py
@@ -1,7 +1,13 @@
+proximo_id = 1
+
 print("--- Informações de Gêneros ---")
 nome_genero = input("Digite o nome do gênero: ")
 
+id_genero = proximo_id
+proximo_id += 1
+
 genero = {
+    "id_genero": id_genero,
     "nome_genero": nome_genero
 }
 
