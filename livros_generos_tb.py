1d7baf421ce699c15f9d80a95165366a153e15a9
Rapha0904 <159035933+Rapha0904@users.noreply.github.com>
2025-09-24 08:49:33 -0300

    Update livros_generos_tb.py

--- livros_generos_tb.py
+++ livros_generos_tb.py
@@ -1,8 +1,14 @@
+proximo_id = 1
+
 print("--- Relação entre Livro e Gênero ---")
 id_livro = input("Digite o ID do livro: ")
 id_genero = input("Digite o ID do gênero: ")
 
+id_livro_genero = proximo_id
+proximo_id += 1
+
 livros_generos = {
+    "id_livro_genero": id_livro_genero,
     "id_livro": id_livro,
     "id_genero": id_genero
 }
