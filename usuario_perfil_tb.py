cd59d2a2dcd1c9b6556f2e54b60231ed6215c53f
Rapha0904 <159035933+Rapha0904@users.noreply.github.com>
2025-09-24 08:47:10 -0300

    Update usuario_perfil_tb.py

--- usuario_perfil_tb.py
+++ usuario_perfil_tb.py
@@ -1,6 +1,9 @@
 import re
 
 print("--- Cadastro de Usuário ---")
+
+proximo_id = 1
+
 nome = input("Digite o nome do usuário: ")
 email = input("Digite o email: ")
 senha = input("Digite a senha: ")
@@ -19,7 +22,11 @@ while True:
 
 caminho_foto = input("Digite o caminho da foto de perfil: ")
 
+id_usuario = proximo_id
+proximo_id += 1
+
 usuario = {
+    "id_usuario": id_usuario,
     "nome": nome,
     "email": email,
     "senha": senha,
