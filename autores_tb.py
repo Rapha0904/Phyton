1cf5bb20435e0985397be73ea1f5225f8b83e046
Rapha0904 <159035933+Rapha0904@users.noreply.github.com>
2025-09-24 08:57:08 -0300

    Update autores_tb.py

--- autores_tb.py
+++ autores_tb.py
@@ -1,8 +1,14 @@
+proximo_id = 1
+
 print("--- Informações do Autor ---")
 nome_completo = input("Digite o nome completo do autor: ")
 biografia = input("Digite a biografia do autor: ")
 
+id_autor = proximo_id
+proximo_id += 1
+
 autor = {
+    "id_autor": id_autor,
     "nome_completo": nome_completo,
     "biografia": biografia
 }
